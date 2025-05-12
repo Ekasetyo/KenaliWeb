514e33a442bebc70afc27f3a37d95fc7ca49a543
fahrezy27 <158013346+fahrezy27@users.noreply.github.com>
2025-05-12 12:34:28 +0700

    update menu user

---
+++ resources/views/user/dashboard-user.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Dashboard">
+    <meta name="author" content="">
+
+    <title>@yield('title', 'Dashboard')</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('dashboard-assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet"
+        type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"  rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('dashboard-assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
+</head>
+
+<body id="page-top">
+
+
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+
+        <!-- Sidebar -->
+        @include('user.layouts.sidebar')
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- navbar -->
+            @include('user.layouts.navbar')
+            <!-- End of navbar -->
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar (jika kamu punya header/topbar bisa ditambahkan di sini) -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    @yield('content') {{-- Ini akan diisi oleh halaman-halaman lain --}}
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Your Website 2025</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('dashboard-assets/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('dashboard-assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset('dashboard-assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset('dashboard-assets/js/sb-admin-2.min.js') }}"></script>
+
+
+    <script src="{{ asset('dashboard-assets/js/script.js') }}"></script>
+
+    @stack('scripts') {{-- Untuk halaman jika ingin menambahkan script khusus --}}
+
+
+
+    
+</body>
+
+</html>
