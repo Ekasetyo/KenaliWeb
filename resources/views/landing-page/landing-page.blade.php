8b318073b904e132319738f0210768c3bd61176e
ramzdhani11 <158116439+ramzdhani11@users.noreply.github.com>
2025-05-16 08:07:57 +0700

    User

--- resources/views/landing-page/landing-page.blade.php
+++ resources/views/landing-page/landing-page.blade.php
@@ -67,19 +67,19 @@
             <div class="container-xxl bg-primary hero-header">
                 <div class="container px-lg-5">
                     <div class="row g-5"> 
-                        <div class="col-lg-8 text-center text-lg-start">
-                            <h1 class="text-white mb-4 animated slideInDown">Solusi Cerdas Untuk Apa?</h1>
-                            <p class="text-white pb-3 animated slideInDown">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt at amet, ut architecto harum laboriosam cum praesentium repudiandae quisquam atque quaerat dolorem voluptate blanditiis doloribus saepe vero fuga, cumque soluta?</p>
+                       <div class="col-lg-8 text-center text-lg-start">
+    <h1 class="text-white mb-4 animated slideInDown">Solusi Cerdas Untuk Apa?</h1>
+    <p class="text-white pb-3 animated slideInDown" style="text-align: justify;">Untuk mengatasi deteksi dini risiko stroke menjadi sangat penting untuk menekan angka kejadian dan mencegah komplikasi serius. Saat ini, skrining risiko stroke umumnya masih mengandalkan pemeriksaan klinis manual dan kuesioner sederhana yang rentan subjektivitas. Selain itu, sumber daya medis di banyak wilayah masih terbatas, sehingga akses ke screening yang komprehensif tidak merata.</p>
                             <!-- <a href="" class="btn btn-primary-gradient py-sm-3 px-4 px-sm-5 rounded-pill me-3 animated slideInLeft">Read More</a> -->
                             <a href="#contact" class="btn btn-secondary-gradient py-sm-3 px-4 px-sm-5 rounded-pill animated slideInRight">Hubungi Kami</a>
                         </div>
                         <div class="col-lg-4 d-flex justify-content-center justify-content-lg-end wow fadeInUp" data-wow-delay="0.3s">
                             <div class="owl-carousel screenshot-carousel">
-                                <img class="img-fluid" src="{{ asset('landing-assets/img/finomali_depan.png') }}" alt="">
-                                <img class="img-fluid" src="{{ asset('landing-assets/img/finomali_depan2.png') }}" alt="">
-                                <img class="img-fluid" src="{{ asset('landing-assets/img/finomali_login.png') }}" alt="">
-                                <img class="img-fluid" src="{{ asset('landing-assets/img/finomali_register.png') }}" alt="">
-                                <img class="img-fluid" src="{{ asset('landing-assets/img/finomali_dashboard.png') }}" alt="">
+                                <img class="img-fluid" src="{{ asset('landing-assets/img/kenali_depan.png') }}" alt="">
+                                <img class="img-fluid" src="{{ asset('landing-assets/img/kenali_aplikasi.png') }}" alt="">
+                                <img class="img-fluid" src="{{ asset('landing-assets/img/kenali_login.png') }}" alt="">
+                                <img class="img-fluid" src="{{ asset('landing-assets/img/kenali_register.png') }}" alt="">
+                                <img class="img-fluid" src="{{ asset('landing-assets/img/kenali_dashboard.png') }}" alt="">
                             </div>
                         </div>
                 </div>
@@ -88,38 +88,38 @@
         <!-- Navbar & Hero End -->
 
         <!-- About Start -->
-        <div class="container-xxl py-5" id="about">
-            <div class="container py-5 px-lg-5">
-                <div class="row g-5 align-items-center">
-                    <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <h5 class="text-primary-gradient fw-medium">Tentang Kenali</h5>
-                        <h1 class="mb-4">#1 </h1>
-                        <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto voluptates corporis delectus qui quasi! Animi totam perspiciatis labore nam at hic eius alias omnis, veritatis excepturi minus quibusdam rerum iste.</p>
-                        <div class="row g-4 mb-4">
-                            <div class="col-sm-6 wow fadeIn" data-wow-delay="0.5s">
-                                <div class="d-flex">
-                                    <i class="fa fa-cogs fa-2x text-primary-gradient flex-shrink-0 mt-1"></i>
-                                    <div class="ms-3">
-                                        <h2 class="mb-0" data-toggle="counter-up">1234</h2>
-                                        <p class="text-primary-gradient mb-0">Pengunduh</p>
-                                    </div>
-                                </div>
+<div class="container-xxl py-5" id="about">
+    <div class="container py-5 px-lg-5">
+        <div class="row g-5 align-items-center">
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                <h5 class="text-primary-gradient fw-medium">Tentang Kenali</h5>
+                <h1 class="mb-4">#1 </h1>
+                <p class="mb-4" style="text-align: justify;">Stroke merupakan penyakit tidak menular yang menjadi penyebab utama kematian dan kecacatan, dan sering kali terjadi secara tiba-tiba tanpa gejala awal yang jelas. Sayangnya, masih banyak masyarakat yang belum menyadari risiko mereka terhadap penyakit ini, serta kurangnya akses terhadap layanan deteksi dini yang cepat dan efisien, terutama di wilayah terpencil. Pengembangan sistem ini diharapkan dapat membantu tenaga medis dalam proses skrining, meningkatkan kesadaran masyarakat akan pentingnya pencegahan stroke, dan pada akhirnya menurunkan angka kejadian stroke melalui deteksi dini berbasis data.</p>
+                <div class="row g-4 mb-4">
+                    <div class="col-sm-6 wow fadeIn" data-wow-delay="0.5s">
+                        <div class="d-flex">
+                            <i class="fa fa-cogs fa-2x text-primary-gradient flex-shrink-0 mt-1"></i>
+                            <div class="ms-3">
+                                <h2 class="mb-0" data-toggle="counter-up">1234</h2>
+                                <p class="text-primary-gradient mb-0">Pengunduh</p>
                             </div>
-                            <div class="col-sm-6 wow fadeIn" data-wow-delay="0.7s">
-                                <div class="d-flex">
-                                    <i class="fa fa-comments fa-2x text-secondary-gradient flex-shrink-0 mt-1"></i>
-                                    <div class="ms-3">
-                                        <h2 class="mb-0" data-toggle="counter-up">1234</h2>
-                                        <p class="text-secondary-gradient mb-0">Reviews</p>
-                                    </div>
-                                </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 wow fadeIn" data-wow-delay="0.7s">
+                        <div class="d-flex">
+                            <i class="fa fa-comments fa-2x text-secondary-gradient flex-shrink-0 mt-1"></i>
+                            <div class="ms-3">
+                                <h2 class="mb-0" data-toggle="counter-up">1234</h2>
+                                <p class="text-secondary-gradient mb-0">Reviews</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- About End -->
+    </div>
+</div>
+<!-- About End -->
 
         <!-- Features Start -->
         <div class="container-xxl py-5" id="feature">
@@ -286,7 +286,10 @@
         </div>
         <!-- BMI Calculator End -->
 
-        <!-- Artikel Start -->
+
+
+
+<!-- Artikel Start -->
         <div class="container-xxl py-5" id="artikel">
             <div class="container py-5 px-lg-5">
                 <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
@@ -296,31 +299,31 @@
                 <div class="row g-4">
                     <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                         <div class="card">
-                            <img class="card-img-top" src="{{ asset('landing-assets/img/article-1.jpg') }}" alt="Artikel 1">
+                            <img class="card-img-top" src="{{ asset('landing-assets/img/stroke1.jpg') }}" alt="Pengertian Stroke" style="height: 220px; object-fit: cover;">
                             <div class="card-body">
-                                <h5 class="card-title">Judul Artikel 1</h5>
-                                <p class="card-text">Ini adalah ringkasan singkat dari artikel pertama. Artikel ini membahas topik yang sangat menarik.</p>
-                                <a href="#" class="btn btn-primary-gradient">Baca Selengkapnya</a>
+                                <h5 class="card-title">Pengertian Stroke</h5>
+                                <p class="card-text" style="text-align: justify;">Artikel ini membahas topik yang sangat menarik, yaitu pengenalan tentang stroke, faktor risiko, dan pentingnya deteksi dini untuk mencegah komplikasi serius.</p>
+                                <a href="https://www.alodokter.com/stroke" target="_blank" class="btn btn-primary-gradient">Baca Selengkapnya</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                         <div class="card">
-                            <img class="card-img-top" src="{{ asset('landing-assets/img/article-2.jpg') }}" alt="Artikel 2">
+                            <img class="card-img-top" src="{{ asset('landing-assets/img/stroke2.jpg') }}" alt="Pengobatan Stroke" style="height: 220px; object-fit: cover;">
                             <div class="card-body">
-                                <h5 class="card-title">Judul Artikel 2</h5>
-                                <p class="card-text">Ini adalah ringkasan singkat dari artikel kedua. Artikel ini memberikan wawasan baru.</p>
-                                <a href="#" class="btn btn-primary-gradient">Baca Selengkapnya</a>
+                                <h5 class="card-title">Pengobatan Stroke</h5>
+                                <p class="card-text" style="text-align: justify;">Bagaimana memberikan wawasan baru mengenai berbagai metode pengobatan stroke, mulai dari penanganan darurat hingga rehabilitasi untuk meningkatkan kualitas.</p>
+                                <a href="https://www.alodokter.com/stroke/pengobatan" target="_blank" class="btn btn-primary-gradient">Baca Selengkapnya</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                         <div class="card">
-                            <img class="card-img-top" src="{{ asset('landing-assets/img/article-3.jpg') }}" alt="Artikel 3">
+                            <img class="card-img-top" src="{{ asset('landing-assets/img/stroke3.jpg') }}" alt="Gejala Stroke" style="height: 220px; object-fit: cover;">
                             <div class="card-body">
-                                <h5 class="card-title">Judul Artikel 3</h5>
-                                <p class="card-text">Ini adalah ringkasan singkat dari artikel ketiga. Artikel ini sangat penting untuk diketahui.</p>
-                                <a href="#" class="btn btn-primary-gradient">Baca Selengkapnya</a>
+                                <h5 class="card-title">Gejala Stroke</h5>
+                                <p class="card-text" style="text-align: justify;">Untuk diketahui karena membahas gejala-gejala stroke yang harus diwaspadai agar penanganan dapat dilakukan secepat mungkin.</p>
+                                <a href="https://www.alodokter.com/stroke/gejala" target="_blank" class="btn btn-primary-gradient">Baca Selengkapnya</a>
                             </div>
                         </div>
                     </div>
